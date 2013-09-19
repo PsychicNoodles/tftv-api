9869b7b8ba7565d102913d597c4210f4533d036a
PsychicNoodles <mattori.birnbaum@gmail.com>
2013-09-19 16:13:00 -0400

    fixed servers.php

--- api/servers.php
+++ api/servers.php
@@ -6,7 +6,7 @@ require "../scripts/simple_html_dom.php";
 
 header("Content-Type: application/json");
 
-$page = file_get_html("http://teamfortress.tv/servers") -> find("div[id=col-center-inner]");
+$page = file_get_html("http://teamfortress.tv/servers") -> find("div[id=col-center-inner]", 0);
 
 $servertype;
 $svrs = array();
@@ -15,20 +15,27 @@ foreach($page -> children() as $child)
 {
     if($child -> class == "table-container")
     {
-        foreach($child -> find("table.list-table server-table", 0) -> children() as $serv)
+        $child = $child -> find("table[class=list-table server-table]", 0);
+        foreach($child -> children() as $serv)
         {
-            if($child -> find("th", 0))
-                $servertype = trim($child -> find("th", 0) -> plaintext);
+            if($serv -> find("th", 0) !== NULL)
+                $servertype = trim($serv -> find("th", 0) -> plaintext);
             else
             {
                 $item = array();
                 $item["Type"] = $servertype;
-                $item["Link"] = $child -> find("a", 0) -> href;
-                $item["Title"] = trim($child -> find("a", 0) -> plaintext);
-                $item["Players"] = trim($child -> find("td", 1) -> plaintext);
-                $item["Map"] = trim($child -> find("td", 2) -> plaintext);
-                $item["Updated"] = trim($child -> find("td", 3) -> plaintext);
-                array_push($svrs, $item);
+                $item["Link"] = $serv -> find("a", 0) -> href;
+                $item["Title"] = trim($serv -> find("a", 0) -> plaintext);
+                $item["Players"] = trim($serv -> find("td", 1) -> plaintext);
+                $item["Map"] = trim($serv -> find("td", 2) -> plaintext);
+                $item["Updated"] = trim($serv -> find("td", 3) -> plaintext);
+                if(isset($_GET["type"]))
+                {
+                    if(strcasecmp($servertype, $_GET["type"]) === 0 || strcasecmp(substr($servertype, 0, strpos($servertype, " ")), $_GET["type"]) === 0)
+                        echo json_encode($item);
+                }
+                else
+                    array_push($svrs, $item);
             }
         }
     }
