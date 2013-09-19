13d438c4d87c3b6a7dbcb19e4035e0908c0810e0
PsychicNoodles <mattori.birnbaum@gmail.com>
2013-09-19 16:18:45 -0400

    fixed streams.php

--- api/streams.php
+++ api/streams.php
@@ -16,8 +16,8 @@ foreach($page -> children as $child)
     $item["Streamer"] = trim($child -> find("b[style=font-size: 12px; color: #007099;]", 0) -> plaintext);
     $item["Viewers"] = trim(str_replace("Viewers", "", $child -> find("span[style=color: #666;]", 0) -> plaintext));
     $item["Link"] = trim($child -> find("a[style=display: inline-block; float: right; color: #666;]", 0) -> href);
-    $item["Title"] = trim(str_replace("Title", "", str_replace("»", "", $child -> find("div[style=padding: 6px; padding-left: 0; padding-right: 18px;]", 0) -> plaintext)));
-    $item["Desc"] = trim(str_replace("Description", "", str_replace("»", "", $child -> find("div[style=padding: 6px; padding-left: 0; padding-right: 18px;]", 1) -> plaintext)));
+    $item["Title"] = trim(str_replace("Title", "", str_replace("&raquo;", "", $child -> find("div[style=padding: 6px; padding-left: 0; padding-right: 18px;]", 0) -> plaintext)));
+    $item["Desc"] = trim(str_replace("Description", "", str_replace("&raquo;", "", $child -> find("div[style=padding: 3px; padding-left: 0; padding-right: 18px;]", 0) -> plaintext)));
     array_push($stms, $item);
 }
 
