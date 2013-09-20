af2e58b3cae04f08790401ec77016b3b87fd3258
PsychicNoodles <mattori.birnbaum@gmail.com>
2013-09-20 10:27:47 -0400

    quick fix to add plaintext instead of raw html for Published

--- api/articlelist.php
+++ api/articlelist.php
@@ -18,7 +18,7 @@ foreach($page -> children() as $child)
         $item["Link"] = $child -> find("a", 0) -> href;
         $item["Title"] = trim($child -> find("a", 0) -> plaintext);
         $item["Author"] = trim(str_replace($item["Title"], "", str_replace("by ", "", $child -> find("td", 0))));
-        $item["Published"] = trim($child -> find("td", 1));
+        $item["Published"] = trim($child -> find("td", 1) -> plaintext);
         $item["Category Link"] = $child -> find("a", 1) -> href;
         $item["Category Title"] = trim($child -> find("a", 1) -> plaintext);
         $item["Series Link"] = $child -> find("a", 2) -> href == "/articles/series/" ? null : $child -> find("a", 2) -> href;
