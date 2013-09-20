ed0365de4afd9a6ee403f8faec06a77bb1d2b126
PsychicNoodles <mattori.birnbaum@gmail.com>
2013-09-20 19:12:18 -0400

    reorganized Title, Link, and Desc to be before everything else

--- api/schedule.php
+++ api/schedule.php
@@ -22,13 +22,13 @@ foreach($page -> children() as $child)
     else if(strpos(trim($child -> plaintext), "START") === 0)
     {
         $item = array(); //split up for easier reading
+        $item["Title"] = trim($child -> find("a", 0) -> plaintext);
+        $item["Link"] = str_replace("\\/", "/", $child -> find("a", 0) -> href);
+        $item["Desc"] = trim($child -> find("div.event-desc", 0) -> plaintext);
         $item["Date"] = strpos(trim($date), "\t") ? substr(trim($date), 0, strpos(trim($date), "\t")) : trim($date); //because explode doesn't work on Heroku for some reason
         $item["Time"] = trim(str_replace("START", "", $child -> find("div[style=padding: 8px;]", 0) -> plaintext));
         $item["Stream"] = trim(str_replace("STREAM", "", $child -> find("td", 1) -> plaintext));
-        $item["Link"] = str_replace("\\/", "/", $child -> find("a", 0) -> href);
-        $item["Title"] = trim($child -> find("a", 0) -> plaintext);
         $item["Flag"] = substr($child -> find("span", 0) -> class, 10); //because explode doesn't work on Heroku for some reason
-        $item["Desc"] = trim($child -> find("div.event-desc", 0) -> plaintext);
         array_push($evts, $item);
     }
     else
