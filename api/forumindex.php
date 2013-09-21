c46830d63894fce0fe11503d976fa4ebaacab90b
PsychicNoodles <mattori.birnbaum@gmail.com>
2013-09-21 15:07:36 -0400

    added forumindex.php

---
+++ api/forumindex.php
@@ -0,0 +1,44 @@
+<?php
+if(!file_exists("../scripts/simple_html_dom.php"))
+    exit("<h1>Internal error, please try again later.</h1>");
+
+require "../scripts/simple_html_dom.php";
+
+header("Content-Type: application/json");
+
+$page = file_get_html("http://teamfortress.tv/forum") -> find("div[id=col-center-inner]", 0);
+
+$forumcat;
+$frms = array();
+
+foreach($page -> children() as $child)
+{
+    if($child -> class == "table-container")
+    {
+        $child = $child -> find("table.list-table", 0);
+        foreach($child -> children() as $sub)
+        {
+            if($sub -> find("th", 0) !== NULL)
+            {
+                $forumcat = trim($sub -> find("a", 0) -> plaintext);
+                array_push($frms, array("Title" => $forumcat, "Link" => $sub -> find("a", 0) -> href));
+            }
+            else
+            {
+                $item = array();
+                $item["Title"] = trim($sub -> find("a", 0) -> plaintext);
+                $item["Link"] = $sub -> find("a", 0) -> href;
+                $item["Threads"] = trim($sub -> find("td", 1) -> plaintext);
+                $item["Posts"] = trim($sub -> find("td", 2) -> plaintext);
+                $item["Last Title"] = trim($sub -> find("a", 1) -> plaintext);
+                $item["Last Link"] = $sub -> find("a", 1) -> href;
+                $item["Last Author"] = trim(str_replace($item["Last Title"], "", substr($sub -> find("td", 4) -> plaintext, 0, strrpos($sub -> find("td", 4) -> plaintext, " posted"))));
+                $item["Last Published"] = trim(substr($sub -> find("td", 4) -> plaintext, strrpos($sub -> find("td", 4) -> plaintext, " posted") + 7));
+                array_push($frms, $item);
+            }
+        }
+    }
+}
+
+echo json_encode($frms);
+?>
