ce3727f5de215f2f180bac24e581641172f22315
PsychicNoodles <mattori.birnbaum@gmail.com>
2013-09-21 15:09:55 -0400

    added api/forumindex.php to index.php

--- index.php
+++ index.php
@@ -8,12 +8,13 @@
         
         <h1>Links</h1>
         
-        <p><a href="api/article.php?page=194-official-ugc-platinum-season-11-predictions-power-rankings-and-player-rankings)">Article (example page: http://teamfortress.tv/articles/view/194-official-ugc-platinum-season-11-predictions-power-rankings-and-player-rankings</a>
+        <p><a href="api/article.php?page=194-official-ugc-platinum-season-11-predictions-power-rankings-and-player-rankings">Article (example page: http://teamfortress.tv/articles/view/194-official-ugc-platinum-season-11-predictions-power-rankings-and-player-rankings)</a>
         <p><a href="api/articlelist.php">Article List</a></p>
         <p><a href="api/event.php?page=349-etf2l-metalworks-one-night-cup">Event (example page: http://teamfortress.tv/schedule/event/349-etf2l-metalworks-one-night-cup)</a></p>
         <p><a href="api/schedule.php">Schedule</a></p>
         <p><a href="api/servers.php">Servers</a></p>
         <p><a href="api/streams.php">Streams</a></p>
+        <p><a href="api/forumindex.php">Forum Index</a></p>
         
         <?php ?>
     </body>
