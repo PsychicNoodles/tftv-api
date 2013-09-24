26c3d4fd6376a8baa1fed5b39184ce761974ef5e
PsychicNoodles <mattori.birnbaum@gmail.com>
2013-09-24 09:47:49 -0400

    added support for comments

--- api/event.php
+++ api/event.php
@@ -11,7 +11,7 @@ if(!isset($_GET["page"]))
     die("\"page\" GET parameter required");
 }
 
-$page = file_get_html("http://teamfortress.tv/schedule/event/" . $_GET["page"]) -> find("table[id=calendar-table]", 0);
+$page = file_get_html("http://teamfortress.tv/schedule/event/" . $_GET["page"]);
 if($page -> find("title", 0) -> plaintext == "Page Cannot be Displayed")
 {
     header("HTTP/1.0 404 Not Found");
@@ -21,7 +21,10 @@ if($page -> find("title", 0) -> plaintext == "Page Cannot be Displayed")
 
 header("Content-Type: application/json");
 
+$evtpage = $page -> find("table[id=calendar-table]", 0);
+$cmtspage = $page -> find("div[id=thread-container]", 0);
 $evt = array();
+$cmts = array();
 
 $evt["Title"] = trim($page -> find("span[id=event-title]", 0) -> plaintext);
 if(strpos($page -> find("span.event-meta", 0) -> plaintext, "Series") == 0)
@@ -49,6 +52,35 @@ else
 $evt["Mumble"] = trim(str_replace("MUMBLE", "", $page -> find("div[style=padding: 8px; border-top: 1px solid #ccc;]", 2) -> plaintext));
 $evt["STV"] = trim(str_replace("STV", "", $page -> find("div[style=padding: 8px; border-top: 1px solid #ccc;]", 3) -> plaintext));
 $evt["Desc"] = trim($page -> find("div[id=event-desc]", 0) -> plaintext);
+
+foreach($cmtspage -> children() as $child)
+{
+    $item = array();
+    $item["ID"] = substr($child -> id, 8);
+    $item["Number"] = trim($child -> find("span.post-num", 0) -> plaintext);
+    $item["Author Name"] = trim($child -> find("a", 2) -> plaintext);
+    $item["Author Link"] = $child -> find("a", 2) -> href;
+    $item["Flag"] = substr($child -> find("span", 1) -> class, 10, 2);
+    $item["Stars"] = (int) substr($child -> find("span", 4) -> class, 5) + (int) substr($child -> find("span", 5) -> class, 5) + (int) substr($child -> find("span", 6) -> class, 5) + (int) substr($child -> find("span", 7) -> class, 5);
+    $item["Frags"] = trim($child -> find("[id=" . $child -> id . "]", 0) -> plaintext);
+    foreach($child -> find("div.post-footer", 0) -> children() as $e)
+        $child -> find("div.post-footer", 0) -> innertext = str_replace($e -> outertext, "", $child -> find("div.post-footer", 0) -> innertext);
+    if(strpos($child -> find("div.post-footer", 0) -> plaintext, "&sdot;") === FALSE)
+    {
+        $item["Published"] = trim(str_replace("Posted", "", $child -> find("div.post-footer", 0) -> plaintext));
+        $item["Edited"] = NULL;
+    }
+    else
+    {
+        $item["Published"] = trim(str_replace("Posted", "", substr($child -> find("div.post-footer", 0) -> plaintext, 0, strrpos($child -> find("div.post-footer", 0) -> plaintext, "&sdot;"))));
+        $item["Edited"] = trim(str_replace("Posted", "", str_replace("&sdot;", "", str_replace($item["Published"], "", $child -> find("div.post-footer", 0) -> plaintext))));
+    }
+    $item["Body Raw"] = trim(htmlentities(str_replace($child -> find("div.post-body", 0) -> outertext, "", $child -> find("div.post-body", 0) -> plaintext)));
+    $item["Body Plaintext"] = trim(htmlentities($child -> find("div.post-body", 0) -> plaintext));
+    array_push($cmts, $item);
+}
+
+$evt["Comments"] = $cmts;
 
 echo json_encode($evt);
 ?>
