2e20c2f3e66b5429c753ff4d11ba8767a298dc69
PsychicNoodles <mattori.birnbaum@gmail.com>
2013-09-24 12:28:54 -0400

    fixed link to api/event.php to be up to date with necessary parameters

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         
         <p><a href="api/article.php?art=194-official-ugc-platinum-season-11-predictions-power-rankings-and-player-rankings">Article (example page: http://teamfortress.tv/articles/view/194-official-ugc-platinum-season-11-predictions-power-rankings-and-player-rankings)</a>
         <p><a href="api/articlelist.php">Article List</a></p>
-        <p><a href="api/event.php?page=349-etf2l-metalworks-one-night-cup">Event (example page: http://teamfortress.tv/schedule/event/349-etf2l-metalworks-one-night-cup)</a></p>
+        <p><a href="api/event.php?evt=349-etf2l-metalworks-one-night-cup">Event (example page: http://teamfortress.tv/schedule/event/349-etf2l-metalworks-one-night-cup)</a></p>
         <p><a href="api/schedule.php">Schedule</a></p>
         <p><a href="api/servers.php">Servers</a></p>
         <p><a href="api/streams.php">Streams</a></p>
