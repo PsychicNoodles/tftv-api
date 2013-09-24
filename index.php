ff3152d2e08f559d34b8bd25802682ac47506af5
PsychicNoodles <mattori.birnbaum@gmail.com>
2013-09-24 13:49:11 -0400

    moved old index to test.php, redirecting index.php to github page

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        
-        <h1>Links</h1>
-        
-        <p><a href="api/article.php?art=194-official-ugc-platinum-season-11-predictions-power-rankings-and-player-rankings">Article (example page: http://teamfortress.tv/articles/view/194-official-ugc-platinum-season-11-predictions-power-rankings-and-player-rankings)</a>
-        <p><a href="api/articlelist.php">Article List</a></p>
-        <p><a href="api/event.php?evt=349-etf2l-metalworks-one-night-cup">Event (example page: http://teamfortress.tv/schedule/event/349-etf2l-metalworks-one-night-cup)</a></p>
-        <p><a href="api/schedule.php">Schedule</a></p>
-        <p><a href="api/servers.php">Servers</a></p>
-        <p><a href="api/streams.php">Streams</a></p>
-        <p><a href="api/forumindex.php">Forum Index</a></p>
-        <p><a href="api/subforum.php?sub=tf2">Subforum (example page: http://teamfortress.tv/forum/tf2/)</a></p>
-        <p><a href="api/thread.php?thr=12566-its-coming">Forum Thread (example page: http://teamfortress.tv/forum/thread/12566-its-coming)</a></p>
-    </body>
-</html>
+<?php
+header("Location: http://psychicnoodles.github.io/tftv-api", $http_response_code = 301);
+?>
