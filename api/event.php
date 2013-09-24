da415f7be774f7431995140c4ddebf94c29c6acd
PsychicNoodles <mattori.birnbaum@gmail.com>
2013-09-24 12:23:32 -0400

    corrected page parsing using the  variable instead of

--- api/event.php
+++ api/event.php
@@ -27,31 +27,32 @@ $evt = array();
 $cmts = array();
 
 $evt["Title"] = trim($page -> find("span[id=event-title]", 0) -> plaintext);
-if(strpos($page -> find("span.event-meta", 0) -> plaintext, "Series") == 0)
+if(strpos($evtpage -> find("span.event-meta", 0) -> plaintext, "Series") == 0)
 {
-    $evt["Region"] = trim($page -> find("span.event-meta", 0) -> find("i", 0) -> plaintext);
-    if(($page -> find("div.e-upcoming", 0) !== NULL))
-        $evt["Time"] = trim(str_replace("START", "", $page -> find("div.e-upcoming", 0) -> plaintext));
+    $evt["Region"] = trim($evtpage -> find("span.event-meta", 0) -> find("i", 0) -> plaintext);
+    if(($evtpage -> find("div.e-upcoming", 0) !== NULL))
+        $evt["Time"] = trim(str_replace("START", "", $evtpage -> find("div.e-upcoming", 0) -> plaintext));
     else
-        $evt["Time"] = trim(str_replace("START", "", $page -> find("div.e-completed", 0) -> plaintext));
+        $evt["Time"] = trim(str_replace("START", "", $evtpage -> find("div.e-completed", 0) -> plaintext));
     $evt["Series"] = null;
 }
 else
 {
-    $evt["Region"] = trim($page -> find("span.event-meta", 1) -> find("i", 0) -> plaintext);
-    if(($page -> find("div.e-upcoming", 0) !== NULL))
-        $evt["Time"] = trim(str_replace("START", "", $page -> find("div.e-upcoming", 0) -> plaintext));
+    $evt["Region"] = trim($evtpage -> find("span.event-meta", 1) -> find("i", 0) -> plaintext);
+    if(($evtpage -> find("div.e-upcoming", 0) !== NULL))
+        $evt["Time"] = trim(str_replace("START", "", $evtpage -> find("div.e-upcoming", 0) -> plaintext));
     else
-        $evt["Time"] = trim(str_replace("START", "", $page -> find("div.e-completed", 0) -> plaintext));}
-$evt["Date"] = trim(substr($page -> find("div[id=event-date]", 0) -> plaintext, 0, strrpos($page -> find("div[id=event-date]", 0) -> plaintext, "Region")));
-$evt["Stream Title"] = trim(str_replace("STREAM", "", $page -> find("div[style=padding: 8px;]", 1) -> plaintext));
-if(strpos($page -> find("div[style=padding: 8px;]", 1) -> plaintext, "N/A") == 0)
-    $evt["Stream Link"] = "http://teamfortress.tv" . $page -> find("div[style=padding: 8px;]", 1) -> find("a", 0) -> href;
+        $evt["Time"] = trim(str_replace("START", "", $evtpage -> find("div.e-completed", 0) -> plaintext));
+}
+$evt["Date"] = trim(substr($evtpage -> find("div[id=event-date]", 0) -> plaintext, 0, strrpos($evtpage -> find("div[id=event-date]", 0) -> plaintext, "Region")));
+$evt["Stream Title"] = trim(str_replace("STREAM", "", $evtpage -> find("div[style=padding: 8px;]", 1) -> plaintext));
+if(strpos($evtpage -> find("div[style=padding: 8px;]", 1) -> plaintext, "N/A") == 0)
+    $evt["Stream Link"] = "http://teamfortress.tv" . $evtpage -> find("div[style=padding: 8px;]", 1) -> find("a", 0) -> href;
 else
     $evt["Stream Link"] = "N/A";
-$evt["Mumble"] = trim(str_replace("MUMBLE", "", $page -> find("div[style=padding: 8px; border-top: 1px solid #ccc;]", 2) -> plaintext));
-$evt["STV"] = trim(str_replace("STV", "", $page -> find("div[style=padding: 8px; border-top: 1px solid #ccc;]", 3) -> plaintext));
-$evt["Desc"] = trim($page -> find("div[id=event-desc]", 0) -> plaintext);
+$evt["Mumble"] = trim(str_replace("MUMBLE", "", $evtpage -> find("div[style=padding: 8px; border-top: 1px solid #ccc;]", 2) -> plaintext));
+$evt["STV"] = trim(str_replace("STV", "", $evtpage -> find("div[style=padding: 8px; border-top: 1px solid #ccc;]", 3) -> plaintext));
+$evt["Desc"] = trim($evtpage -> find("div[id=event-desc]", 0) -> plaintext);
 
 foreach($cmtspage -> children() as $child)
 {
